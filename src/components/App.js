9905990e05b19e9488deecbcdcf006e61c5a5d72
JangSoJin <wkdthwls3@naver.com>
2017-07-21 03:51:04 +0900

    과제1(30%)

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+import Connetcounter from './Connetcounter';
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <Connetcounter/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Connetcounter.js
@@ -0,0 +1,46 @@
+import Counter from '../components/Counter';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => ({
+    color: state.color,
+    number: state.number
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onIncrement: () => dispatch(actions.increment()),
+    onDecrement: () => dispatch(actions.decrement()),
+    onSetColor: () => {
+        const color = getRandomColor();
+        dispatch(actions.setColor(color)); //*
+    }
+});
+
+const Connetcounter = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
+
+function getRandomColor() { //*
+    const colors = [
+        '#495057',
+        '#f03e3e',
+        '#d6336c',
+        '#ae3ec9',
+        '#7048e8',
+        '#4263eb',
+        '#1c7cd6',
+        '#1098ad',
+        '#0ca678',
+        '#37b24d',
+        '#74b816',
+        '#f59f00',
+        '#f76707'
+    ];
+
+    const random = Math.floor(Math.random() * 13);
+
+    return colors[random];
+}
+
+export default Connetcounter;
